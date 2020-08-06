b8ba861f2f1eb2bc6e6468cf31d683b1852b0339
Tea Mitchell <teamitchell@rocketmail.com>
2020-08-06 17:23:56 -0500

    completed extra hard mode

--- reactApps.js
+++ reactApps.js
@@ -33,6 +33,24 @@ function Team(props){
         )
 }
 
+function ScoreBoard(props){
+    return(
+        <div className="ScoreBoard">
+            <div className="teamStats">
+                <h3>Home</h3>
+                <h3>{props.homeTeamStats.score}</h3>
+            </div>
+
+            <h3>ScoreBoard</h3>
+
+            <div className="teamStats">
+                <h3>Visiting</h3>
+                <h3>{props.visitingTeamStats.score}</h3>
+            </div>
+        </div>
+    )
+}
+
 class Game extends React.Component {
     constructor(props) {
         super(props)
@@ -92,6 +110,10 @@ class Game extends React.Component {
     render() {
         return (
             <div className="Game">
+                <ScoreBoard 
+                    homeTeamStats={this.state.homeTeamStats}
+                    visitingTeamStats={this.state.visitingTeamStats}
+                />
 
                 <h1>Welcome to {this.props.venue}</h1>
                 <div className="stats">
